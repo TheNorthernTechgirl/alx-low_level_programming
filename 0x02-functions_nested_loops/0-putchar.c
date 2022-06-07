8a5fc13513ad791322917fcfe15f2cc9a0a55fe1
khadija <khadijamusa1112gmail.com>
2022-06-07 14:25:21 -0700

    task on functions and loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - prints string "_putchar" using
+ * a header file and putchar() function prototype defined in it 
+ * Return; Always 0 (Success)
+ */
+int main(void)
+{
+	char y[] = "_putchar";
+	int i;
+
+	for (i = 0; i < 8; i++)
+	{
+		put char(y[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
