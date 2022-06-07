74f970eeb670414f6519198c447721c450b4a885
khadija <khadijamusa1112gmail.com>
2022-06-07 15:50:36 -0700

    print putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * main - prints string "_putchar" using
- * a header file and putchar() function prototype defined in it 
- * Return; Always 0 (Success)
+ * main - prints string _putchar using
+ * a header file and putchar() function prototype defined in it
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char y[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		put char(y[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
